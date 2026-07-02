99137f62e76d79fbab8a473dcaca7025d0670343
Mhdrashid-crypto <rashidthangal123@gamil.com>
2026-07-02 19:50:30 +0530

    update email record

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Auth/Login/login"
 import Dashboard from "./Pages/Dashboard/dashboard";
 
-import EmailRecord from "./Pages/EmailRecord";
+import EmailRecord from "./Pages/Leads/components/EmailRecord";
 import Leadslist from "./Pages/Leads/Leadslist";
 import CompanyDetails from "./Pages/Companies/CompanyDetails/CompanyDetails";
 import CreateTicket from "./Pages/Tickets/components/CreateTicket"; 
