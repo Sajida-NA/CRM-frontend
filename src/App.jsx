6a86794bf9e4e239d0b6b0fb8bfc6540b4e07d73
sajida <sajijubi2423@gmail.com>
2026-07-03 10:07:28 +0400

    update application routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Pages/Auth/Login/login"
+
+import Login from "./Pages/Auth/Login/login";
 import Dashboard from "./Pages/Dashboard/dashboard";
 
 import EmailRecord from "./Pages/Leads/components/EmailRecord";
 import Leadslist from "./Pages/Leads/Leadslist";
+
 import CompanyDetails from "./Pages/Companies/CompanyDetails/CompanyDetails";
 import CreateTicket from "./Pages/Tickets/components/CreateTicket"; 
 import ForgotPassword from "./Pages/Auth/ForgotPassword/ForgotPassword";
-import DealsList from "./Pages/Deals/DealsList"
+
+// FIXED IMPORTS
+// import DealsList from "./Pages/Deals/DealsList"
+// import DealProfile from "./Pages/Deals/DealProfile";
 
 function App() {
   return (
-    <>
     <BrowserRouter>
       <Routes>
         <Route path="/emailrecord" element={<EmailRecord />} />  
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/CreateTicket" element={<CreateTicket/>}/>
+        <Route path="/CreateTicket" element={<CreateTicket />} />
         <Route path="/company/:id" element={<CompanyDetails />} />
-        <Route path="/leadslist" element={<Leadslist/>}/>
-        <Route path="/forgotpassword" element={<ForgotPassword/>}/>
-        <Route path="/dealslist" element={<DealsList/>}/>
-         <Route path="/dealsprofile" element={<DealProfile/>}/>
-        {/* <Route path="/register" element={<Register/>}/> */}
+        <Route path="/leadslist" element={<Leadslist />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        {/* <Route path="/createdealmodal" element={<CreateDealModal />} /> */}
+        {/* FIXED ROUTES */}
+        {/* <Route path="/dealslist" element={<DealsList />} /> */}
+        {/* <Route path="/dealprofile/:id" element={<DealProfile />} /> */}
       </Routes>
     </BrowserRouter>
-    </>
   );
-
 }
 
 export default App;
