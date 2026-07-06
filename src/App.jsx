07378a9f8366eae8cf59ceb63e5c5bcda0721560
Abina <abinaaziz@gmail.com>
2026-07-06 14:14:13 +0530

    Created ticket list

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Dashboard from "./Pages/Dashboard/dashboard";
 import EmailRecord from "./Pages/Leads/components/EmailRecord";
 import Leadslist from "./Pages/Leads/Leadslist";
 import CompanyDetails from "./Pages/Companies/CompanyDetails/CompanyDetails";
-import CreateTicket from "./Pages/Tickets/components/CreateTicket"; 
+
 import ForgotPassword from "./Pages/Auth/ForgotPassword/ForgotPassword";
 import CompaniesList from "./Pages/Companies/CompaniesList/CompaniesList";
 import Register from "./Pages/Auth/Register/Register";
+import TicketsList from "./Pages/Tickets/TicketsList/TicketsList";
 
 
 function App() {
@@ -18,12 +19,13 @@ function App() {
         <Route path="/emailrecord" element={<EmailRecord />} />  
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/CreateTicket" element={<CreateTicket/>}/>
+       
         <Route path="/company/:id" element={<CompanyDetails />} />
         <Route path="/leadslist" element={<Leadslist/>}/>
         <Route path="/forgotpassword" element={<ForgotPassword/>}/>
         <Route path="/companieslist" element={<CompaniesList />} />
         <Route path="/register" element={<Register/>}/>
+        <Route path="/ticketslist" element={<TicketsList/>}/>
       </Routes>
     </BrowserRouter>
     </>
