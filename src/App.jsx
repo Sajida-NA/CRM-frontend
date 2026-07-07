976e18abe12f122ede9c79f7cb11574c3269fca2
Abina <abinaaziz@gmail.com>
2026-07-07 13:20:56 +0530

    Resolve

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Dashboard from "./Pages/Dashboard/dashboard";
 import EmailRecord from "./Pages/Leads/components/EmailRecord";
 import Leadslist from "./Pages/Leads/LeadsList/Leadslist";
 import CompanyDetails from "./Pages/Companies/CompanyDetails/CompanyDetails";
-import CreateTicket from "./Pages/Tickets/components/CreateTicket"; 
-import ForgotPassword from "./Pages/Auth/ForgotPassword/ForgotPassword";
-import CompaniesList from "./Pages/Companies/CompaniesList/CompaniesList";
-import ViewEmailRecords from "./Pages/Leads/ViewEmailRecords/ViewEmailRecords";
 
 import ForgotPassword from "./Pages/Auth/ForgotPassword/ForgotPassword";
+
+import ViewEmailRecords from "./Pages/Leads/ViewEmailRecords/ViewEmailRecords";
+
+
 import TicketProfile from "./Pages/Ticket/TicketProfile/TicketProfile";
 import CompaniesList from "./Pages/Companies/CompaniesList/CompaniesList";
 import Register from "./Pages/Auth/Register/Register";
@@ -33,12 +33,14 @@ function App() {
         <Route path="/emailrecord" element={<EmailRecord />} />  
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/CreateTicket" element={<CreateTicket/>}/>
+        
         <Route path="/company/:id" element={<CompanyDetails />} />
         <Route path="/leadslist" element={<Leadslist/>}/>
         <Route path="/forgotpassword" element={<ForgotPassword/>}/>
         <Route path="/companieslist" element={<CompaniesList />} />
-        {/* <Route path="/register" element={<Register/>}/> */}
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/ticketslist" element={<TicketsList/>}/>
+
       </Routes>
     </BrowserRouter>
     </>
