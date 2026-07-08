5803341375eafb2eefb4f2ebd637e6faa9e73074
sajida <sajijubi2423@gmail.com>
2026-07-08 11:35:04 +0400

    move route definitions to AppRoutes component

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,46 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "../Pages/Auth/Login/login";
+import Dashboard from "../Pages/Dashboard/dashboard";
+import EmailRecord from "../Pages/Leads/components/EmailRecord";
+import Leadslist from "../Pages/Leads/LeadsList/Leadslist";
+import CompanyProfile from "../Pages/Companies/CompanyProfile/CompanyProfile";
+import CreateTicketDrawer from "../Pages/Tickets/components/CreateTicketDrawer";
+import ForgotPassword from "../Pages/Auth/ForgotPassword/ForgotPassword";
+import TicketProfile from "../Pages/Tickets/TicketProfile/TicketProfile";
+import CompaniesList from "../Pages/Companies/CompaniesList/CompaniesList";
+import Register from "../Pages/Auth/Register/Register";
+import TicketsList from "../Pages/Tickets/TicketsList/TicketsList";
+import DealsList from "../Pages/Deals/DealsList/DealsList";
+
+function AppRoutes() {
+  return (
+    <Routes>
+      {/* Authentication Routes */}
+      <Route path="/" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* Dashboard Route */}
+      <Route path="/dashboard" element={<Dashboard />} />
+
+      {/* Lead Routes */}
+      <Route path="/leadslist" element={<Leadslist />} />
+      <Route path="/emailrecord" element={<EmailRecord />} />
+
+      {/* Company Routes */}
+      <Route path="/companieslist" element={<CompaniesList />} />
+      <Route path="/company-profile" element={<CompanyProfile />} />
+
+      {/* Ticket Routes */}
+      <Route path="/ticketslist" element={<TicketsList />} />
+      <Route path="/ticket-profile" element={<TicketProfile />} />
+      <Route path="/create-ticket" element={<CreateTicketDrawer />} />
+
+      {/* Deal Routes */}
+      <Route path="/dealslist" element={<DealsList />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
