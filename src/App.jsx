bb4979319c7ba3557a57b1b8518ba1171b3b443a
Abina <abinaaziz@gmail.com>
2026-07-09 14:38:48 +0530

    Created a lead note page

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ import TicketsList from "./Pages/Tickets/TicketsList/TicketsList";
 import DealsList from "./Pages/Deals/DealsList/DealsList";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import Notetab from "./Pages/Leads/components/Tabs/Notetab";
+import CommonEntityHeader from "./Components/common/CommonEntityHeader";
 
 // import theme from "./theme";
 // import { ThemeProvider } from "@mui/material";
@@ -35,6 +37,9 @@ function App() {
           <Route path="/companieslist" element={<CompaniesList />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dealslist" element={<DealsList />} />
+          <Route path="/notetab" element={<Notetab/>}/>
+
+          <Route path="/entity" element={<CommonEntityHeader/>}/>
         </Routes>
       </BrowserRouter>
         {/* </ThemeProvider> */}
