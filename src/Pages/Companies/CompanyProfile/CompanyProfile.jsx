55e372c6860016bca354669f7827ba27a0f73a01
shibi <puthramannilshibin@gmail.com>
2026-07-14 14:54:41 +0530

    updated compant and ticket profile

--- src/Pages/Companies/CompanyProfile/CompanyProfile.jsx
+++ src/Pages/Companies/CompanyProfile/CompanyProfile.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 import ProfileLayout from "../../../Components/Profile/ProfileLayout";
 import companyProfileData from "./companyProfileData";
+import { Box } from "@mui/material";
 
 export default function CompanyProfile() {
   return (
+    
     <ProfileLayout
       title="Companies"
       entityType="company"
       profileData={companyProfileData}
+      
     />
+    
   );
 }
