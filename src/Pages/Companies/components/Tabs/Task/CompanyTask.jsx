5bb21904de91d452a102660312aa819cf45568e5
Afnitha <afnitharishad25@gmail.com>
2026-07-18 19:33:07 +0530

    updated company task

--- src/Pages/Companies/components/Tabs/Task/CompanyTask.jsx
+++ src/Pages/Companies/components/Tabs/Task/CompanyTask.jsx
@@ -5,14 +5,7 @@ import TaskDetails from "../../../../Leads/components/Tabs/Task/TaskDetails";
 export default function CompanyTask() {
   return (
     <div>
-<<<<<<< HEAD
       <CompanyLeftPanel>  <TaskDetails/></CompanyLeftPanel>
      </div>
   )
-=======
-      <CompanyLeftPanel />
-      <TaskDetails />
-    </div>
-  );
->>>>>>> 543b2241ef27c4d7f6dfe3c057b64c3bd8695efb
 }
