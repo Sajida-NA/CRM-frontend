82a7e150147c320b6d50ddd82aa6d101eeb57d03
sajida <sajijubi2423@gmail.com>
2026-09-02 20:13:47 +0400

    fix(common): update phone input

--- src/Components/common/PhoneInputField.jsx
+++ src/Components/common/PhoneInputField.jsx
@@ -1,75 +1,3 @@
-// import {
-//   Box,
-//   TextField,
-//   InputAdornment,
-//   Typography,
-//   MenuItem,
-// } from "@mui/material";
-// import { useState } from "react";
-
-// export default function PhoneInputField({
-//   label,
-//   required = false,
-//   value,
-//   onChange,
-//   name,
-// }) {
-//   const [countryCode, setCountryCode] = useState("+91");
-
-//   return (
-//     <Box>
-//       <Typography
-//         sx={{
-//           fontSize: 14,
-//           fontWeight: 600,
-//           mb: 1,
-//           color: "#344054",
-//         }}
-//       >
-//         {label}
-//         {required && (
-//           <Box component="span" sx={{ color: "#F04438", ml: 0.5 }}>
-//             *
-//           </Box>
-//         )}
-//       </Typography>
-
-//       <TextField
-//         fullWidth
-//         placeholder="Enter"
-//         name={name}
-//         value={value}
-//         onChange={onChange}
-//         InputProps={{
-//           startAdornment: (
-//             <InputAdornment position="start">
-//               <TextField
-//                 select
-//                 value={countryCode}
-//                 onChange={(e) => setCountryCode(e.target.value)}
-//                 variant="standard"
-//                 sx={{ width: 90 }}
-//               >
-//                 <MenuItem value="+91">🇮🇳 +91</MenuItem>
-//                 <MenuItem value="+1">🇺🇸 +1</MenuItem>
-//                 <MenuItem value="+44">🇬🇧 +44</MenuItem>
-//               </TextField>
-//             </InputAdornment>
-//           ),
-//         }}
-//         sx={{
-//           "& .MuiOutlinedInput-root": {
-//             borderRadius: "10px",
-//             height: 44,
-//           },
-//         }}
-//       />
-//     </Box>
-//   );
-// }
-
-
-
 import {
   Box,
   TextField,
@@ -139,38 +67,41 @@ export default function PhoneInputField({
         name={name}
         value={value}
         onChange={handlePhoneChange}
-        inputProps={{
-          inputMode: "numeric",
-          maxLength: 12,
-        }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <TextField
-                select
-                value={countryCode}
-                onChange={(e) =>
-                  setCountryCode(e.target.value)
-                }
-                variant="standard"
-                sx={{
-                  width: 90,
-                }}
-              >
-                <MenuItem value="+91">
-                  🇮🇳 +91
-                </MenuItem>
+        slotProps={{
+          htmlInput: {
+            inputMode: "numeric",
+            maxLength: 12,
+          },
 
-                <MenuItem value="+1">
-                  🇺🇸 +1
-                </MenuItem>
+          input: {
+            startAdornment: (
+              <InputAdornment position="start">
+                <TextField
+                  select
+                  value={countryCode}
+                  onChange={(e) =>
+                    setCountryCode(e.target.value)
+                  }
+                  variant="standard"
+                  sx={{
+                    width: 90,
+                  }}
+                >
+                  <MenuItem value="+91">
+                    🇮🇳 +91
+                  </MenuItem>
 
-                <MenuItem value="+44">
-                  🇬🇧 +44
-                </MenuItem>
-              </TextField>
-            </InputAdornment>
-          ),
+                  <MenuItem value="+1">
+                    🇺🇸 +1
+                  </MenuItem>
+
+                  <MenuItem value="+44">
+                    🇬🇧 +44
+                  </MenuItem>
+                </TextField>
+              </InputAdornment>
+            ),
+          },
         }}
         sx={{
           "& .MuiOutlinedInput-root": {
@@ -182,3 +113,4 @@ export default function PhoneInputField({
     </Box>
   );
 }
+
